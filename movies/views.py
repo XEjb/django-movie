719841080e70569d8bdd0e006603a39772b3e4ef
Xejb <vladiq-artamonenko@rambler.ru>
2023-03-13 15:51:46 +0300

    adding a rating to a movie

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,11 @@
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor, Genre
-from .forms import ReviewForm
+from .models import Movie, Category, Actor, Genre, Rating
+from .forms import ReviewForm, RatingForm
 
 
 class GenreYear:
@@ -30,6 +30,11 @@ class MovieDetailView(GenreYear, DetailView):
 
     model = Movie
     slug_field = 'url'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['star_form'] = RatingForm()
+        return context
 
 
 class AddReview(View):
